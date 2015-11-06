b288714e20c5175883fa57567809c0566cbc5ef7
sungoodboy <361574485@qq.com>
2015-11-07 00:42:21 +0800

    Add LoginActivity & login.xml

--- gen/com/duang/easyecard/R.java
+++ gen/com/duang/easyecard/R.java
@@ -52,58 +52,74 @@ containing a value of this type.
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int emo_im_cool=0x7f020000;
-        public static final int emo_im_crying=0x7f020001;
-        public static final int emo_im_embarrassed=0x7f020002;
-        public static final int emo_im_foot_in_mouth=0x7f020003;
-        public static final int emo_im_happy=0x7f020004;
-        public static final int emo_im_kissing=0x7f020005;
-        public static final int emo_im_laughing=0x7f020006;
-        public static final int emo_im_lips_are_sealed=0x7f020007;
-        public static final int emo_im_money_mouth=0x7f020008;
-        public static final int emo_im_sad=0x7f020009;
-        public static final int emo_im_surprised=0x7f02000a;
-        public static final int emo_im_tongue_sticking_out=0x7f02000b;
-        public static final int emo_im_undecided=0x7f02000c;
-        public static final int emo_im_winking=0x7f02000d;
-        public static final int emo_im_wtf=0x7f02000e;
-        public static final int emo_im_yelling=0x7f02000f;
-        public static final int ic_launcher=0x7f020010;
-        public static final int ic_menu_compass=0x7f020011;
-        public static final int ic_menu_emoticons=0x7f020012;
-        public static final int ic_menu_home=0x7f020013;
-        public static final int ic_menu_search=0x7f020014;
-        public static final int tab_bg=0x7f020015;
+        public static final int background_login=0x7f020000;
+        public static final int background_login_div_bg=0x7f020001;
+        public static final int emo_im_cool=0x7f020002;
+        public static final int emo_im_crying=0x7f020003;
+        public static final int emo_im_embarrassed=0x7f020004;
+        public static final int emo_im_foot_in_mouth=0x7f020005;
+        public static final int emo_im_happy=0x7f020006;
+        public static final int emo_im_kissing=0x7f020007;
+        public static final int emo_im_laughing=0x7f020008;
+        public static final int emo_im_lips_are_sealed=0x7f020009;
+        public static final int emo_im_money_mouth=0x7f02000a;
+        public static final int emo_im_sad=0x7f02000b;
+        public static final int emo_im_surprised=0x7f02000c;
+        public static final int emo_im_tongue_sticking_out=0x7f02000d;
+        public static final int emo_im_undecided=0x7f02000e;
+        public static final int emo_im_winking=0x7f02000f;
+        public static final int emo_im_wtf=0x7f020010;
+        public static final int emo_im_yelling=0x7f020011;
+        public static final int ic_launcher=0x7f020012;
+        public static final int ic_menu_compass=0x7f020013;
+        public static final int ic_menu_emoticons=0x7f020014;
+        public static final int ic_menu_home=0x7f020015;
+        public static final int ic_menu_search=0x7f020016;
+        public static final int tab_bg=0x7f020017;
     }
     public static final class id {
-        public static final int action_add_friend=0x7f080007;
-        public static final int action_feedback=0x7f080009;
-        public static final int action_group_chat=0x7f080006;
-        public static final int action_scan=0x7f080008;
-        public static final int action_search=0x7f080005;
+        public static final int action_add_friend=0x7f08000f;
+        public static final int action_feedback=0x7f080011;
+        public static final int action_group_chat=0x7f08000e;
+        public static final int action_scan=0x7f080010;
+        public static final int action_search=0x7f08000d;
         public static final int id_indicator_four=0x7f080004;
         public static final int id_indicator_one=0x7f080001;
         public static final int id_indicator_three=0x7f080003;
         public static final int id_indicator_two=0x7f080002;
         public static final int id_viewpager=0x7f080000;
+        public static final int login_div=0x7f080005;
+        public static final int login_password_input=0x7f080008;
+        public static final int login_user_input=0x7f080006;
+        public static final int miniTwitter_logo=0x7f08000c;
+        public static final int password_edit=0x7f080009;
+        public static final int register_link=0x7f08000b;
+        public static final int signin_button=0x7f08000a;
+        public static final int username_edit=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int login=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_add=0x7f050002;
-        public static final int action_search=0x7f050001;
+        public static final int action_add=0x7f050007;
+        public static final int action_search=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int menu_addfriend=0x7f050005;
-        public static final int menu_feedback=0x7f050004;
-        public static final int menu_group_chat=0x7f050003;
-        public static final int menu_scan=0x7f050006;
-        public static final int tab_found=0x7f050008;
-        public static final int tab_lost=0x7f050007;
-        public static final int tab_me=0x7f050009;
+        public static final int login_label_password=0x7f050003;
+        public static final int login_label_signin=0x7f050004;
+        public static final int login_label_username=0x7f050001;
+        public static final int login_register_link=0x7f050005;
+        public static final int login_username_hint=0x7f050002;
+        public static final int menu_addfriend=0x7f05000a;
+        public static final int menu_feedback=0x7f050009;
+        public static final int menu_group_chat=0x7f050008;
+        public static final int menu_scan=0x7f05000b;
+        public static final int tab_found=0x7f05000d;
+        public static final int tab_lost=0x7f05000c;
+        public static final int tab_me=0x7f05000e;
     }
     public static final class style {
         /** 
