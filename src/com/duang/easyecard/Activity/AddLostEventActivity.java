d8ac85200f34c68f8215156758cc0435b8c5f25d
sungoodboy <361574485@qq.com>
2015-11-11 11:01:44 +0800

    继续优化add_lost_event的布局，去掉了标题栏，添加了控件作为标题。

--- src/com/duang/easyecard/Activity/AddLostEventActivity.java
+++ src/com/duang/easyecard/Activity/AddLostEventActivity.java
@@ -3,11 +3,14 @@ package com.duang.easyecard.Activity;
 import com.duang.easyecard.R;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class AddLostEventActivity extends BaseActivity	{
 
 	public void onCreate(Bundle savedInstanceState)	{
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.add_lost_event);
+		
 	}
 }
