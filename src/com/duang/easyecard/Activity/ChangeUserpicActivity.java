2cdd441140120c4ad227d72fc32663b457835273
SunGoodBoy <361574485@qq.com>
2015-11-24 00:31:00 +0800

    Choose picture from Album, but failed. it may be caused by the bitmap is too big for the picture in album.

--- src/com/duang/easyecard/Activity/ChangeUserpicActivity.java
+++ src/com/duang/easyecard/Activity/ChangeUserpicActivity.java
@@ -9,7 +9,6 @@ import com.duang.easyecard.R;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Picture;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -42,6 +41,7 @@ public class ChangeUserpicActivity extends BaseActivity {
 		chooseFromAlbumButton = (Button) findViewById(R.id.change_userpic_choose_from_album);
 		cancelButton = (Button) findViewById(R.id.change_userpic_cancel);
 		
+		// 设置“拍照”按钮的点击事件
 		takePhotoButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -60,6 +60,42 @@ public class ChangeUserpicActivity extends BaseActivity {
 				Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
 				intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
 				startActivityForResult(intent, TAKE_PHOTO);  //启动相机程序
+			}
+		});
+		
+		// 设置“从相册中选取”按钮的点击事件
+		chooseFromAlbumButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// 创建File对象，用于存储拍照后的图片
+				File outputImage = new File(Environment.getExternalStorageDirectory(),
+						"output_image.jpg");
+				try {
+					if (outputImage.exists()) {
+						outputImage.delete();
+					}
+					outputImage.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				imageUri = Uri.fromFile(outputImage);
+				Intent intent = new Intent("android.intent.action.GET_CONTENT");
+				intent.setType("image/*");
+				intent.putExtra("crop", true);
+				intent.putExtra("scale", true);
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+				startActivityForResult(intent, CROP_PHOTO);
+			}
+		});
+		
+		// 设置取消按钮的点击事件
+		cancelButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
 			}
 		});
 	}
